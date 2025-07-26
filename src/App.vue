0ebcc976c02bd16666f7ffccce0870f65a08105d
Hoxton <za546321@yahoo.com.tw>
2025-07-27 04:30:57 +0800

    2025-07-27 commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="d-flex">
+  <div class="d-flex app-container">
     <Sidebar />
-    <div class="flex-grow-1 p-4">
+    <div class="flex-grow-1 p-4 main-content">
       <router-view />
     </div>
   </div>
@@ -10,3 +10,24 @@
 <script setup>
 import Sidebar from './components/Sidebar.vue'
 </script>
+
+<style>
+/* 設定整頁最大尺寸與隱藏水平溢出 */
+html, body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  overflow-x: hidden;
+}
+
+.app-container {
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+}
+
+.main-content {
+  overflow-y: auto;
+  height: 100vh;
+}
+</style>
